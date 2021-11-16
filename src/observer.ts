0e4fd9caf567053da2521fb0df5643b42d85649c
wurenbieke <wurenbieke@bytedance.com>
2021-11-16 17:38:09 +0800

    feat: observer function should be generic function

--- src/observer.ts
+++ src/observer.ts
@@ -1,30 +1,30 @@
 import { StateMachineConstructor } from './state-machine'
 import { ListenersLifeCycleEventType } from './life-cycle'
 
-export function observer(instance: InstanceType<StateMachineConstructor>) {
+export function observer<T extends InstanceType<StateMachineConstructor>>(instance: T) {
   const listeners: Array<{ type: ListenersLifeCycleEventType; callback: (...args: any[]) => any }> = []
   return {
-    onBeforeTransition(callback: Parameters<typeof instance.addEventListener>[1]) {
+    onBeforeTransition(callback: Parameters<T['addEventListener']>[1]) {
       instance.addEventListener('onBeforeTransition', callback)
       listeners.push({ type: 'onBeforeTransition', callback })
       return this
     },
-    onLeaveState(callback: Parameters<typeof instance.addEventListener>[1]) {
+    onLeaveState(callback: Parameters<T['addEventListener']>[1]) {
       instance.addEventListener('onLeaveState', callback)
       listeners.push({ type: 'onLeaveState', callback })
       return this
     },
-    onTransition(callback: Parameters<typeof instance.addEventListener>[1]) {
+    onTransition(callback: Parameters<T['addEventListener']>[1]) {
       instance.addEventListener('onTransition', callback)
       listeners.push({ type: 'onTransition', callback })
       return this
     },
-    onEnterState(callback: Parameters<typeof instance.addEventListener>[1]) {
+    onEnterState(callback: Parameters<T['addEventListener']>[1]) {
       instance.addEventListener('onEnterState', callback)
       listeners.push({ type: 'onEnterState', callback })
       return this
     },
-    onAfterTransition(callback: Parameters<typeof instance.addEventListener>[1]) {
+    onAfterTransition(callback: Parameters<T['addEventListener']>[1]) {
       instance.addEventListener('onAfterTransition', callback)
       listeners.push({ type: 'onAfterTransition', callback })
       return this
